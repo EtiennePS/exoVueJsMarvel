8369550fdcc327cbd0e72c5b632f087e40459105
EtiennePS <etienne.picherit@gmail.com>
2020-10-20 23:45:51 +0200

    Ajout d'une image dans le composant 404

--- src/components/EntityNotFound.vue
+++ src/components/EntityNotFound.vue
@@ -1,7 +1,9 @@
+/* eslint-disable prettier/prettier */
 <template lang="pug">
 .entityNotFound
   h1 404
   p The {{ entityName }} you are looking for can't be found. Let's hope it hasn't been snapped!
+  img.notFound(src="@/assets/img/404.jpg")
 </template>
 
 <script>
@@ -12,3 +14,24 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.entityNotFound {
+  position: relative;
+}
+.entityNotFound h1 {
+  font-size: 3em;
+}
+.entityNotFound p {
+  width: 80%;
+  margin: auto;
+}
+img.notFound {
+  width: 30%;
+  max-width: 400px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+</style>
